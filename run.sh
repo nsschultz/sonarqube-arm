55a074e0d76e40b47a3b675986d29c6b23628dcb
Nick Schultz <nschultz@nvisia.com>
2022-04-03 13:51:50 -0500

    add old options back

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
-exec java -jar lib/sonar-application-"${SONAR_VERSION}".jar -Dsonar.log.console=true
+exec su-exec sonarqube \
+  java -jar lib/sonar-application-$SONAR_VERSION.jar \
+  -Dsonar.log.console=true \
+  -Dsonar.jdbc.username="$SONARQUBE_JDBC_USERNAME" \
+  -Dsonar.jdbc.password="$SONARQUBE_JDBC_PASSWORD" \
+  -Dsonar.jdbc.url="$SONARQUBE_JDBC_URL" \
+  -Dsonar.search.javaAdditionalOpts=-Dnode.store.allow_mmapfs=false \
+  -Dsonar.web.javaAdditionalOpts="$SONARQUBE_WEB_JVM_OPTS -Djava.security.egd=file:/dev/./urandom"
