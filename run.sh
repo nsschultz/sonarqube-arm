44935140eb95b312e29cc0cf6a7f256c7ff7d272
Nick Schultz <nschultz@nvisia.com>
2022-04-03 13:40:39 -0500

    remove other options

--- run.sh
+++ run.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 set -euo pipefail
-if [ "${1:0:1}" != '-' ]; then
-  exec "$@"
-fi
-exec java -jar lib/sonar-application-"${SONAR_VERSION}".jar -Dsonar.log.console=true "$@"
+exec java -jar lib/sonar-application-"${SONAR_VERSION}".jar -Dsonar.log.console=true
